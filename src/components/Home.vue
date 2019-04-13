39b05d089603f7624b9b79007e3858a6b7578d4d
gelory <917589697@qq.com>
2019-04-13 20:43:18 +0800

    '样式修改'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home" :style="{width:curWidth+'px',height:curHeight+'px'}">
     <img class="fullImage" src="../../image/home_background.png">
     <div class="content" :style="{width:curWidth+'px',height:curHeight+'px'}">
       <div class="titleBox">
@@ -9,32 +9,32 @@
       <div class="swiperBox">
         <swiper :options="swiperOption" autoplay="true" class="wiper">
           <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">
-              <img :src="require(`../../image/Carousel${index+1}.png`)" class="tabImage"/>
+            <img :src="require(`../../image/Carousel${index+1}.png`)" class="tabImage">
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
       </div>
       <div class="iconButton">
-          <div class="iconBox">
-              <img src="../../image/workout.png"/>
-              <div>销售锻炼</div>
-          </div>
-          <div class="iconBox">
-              <img src="../../image/cultivate.png"/>
-              <div>习惯养成</div>
-          </div>
-          <div class="iconBox">
-              <img src="../../image/3tu_03.png"/>
-              <div>能力培养</div>
-          </div>
+        <div class="iconBox">
+          <img src="../../image/workout.png">
+          <div>销售锻炼</div>
+        </div>
+        <div class="iconBox">
+          <img src="../../image/cultivate.png">
+          <div>习惯养成</div>
+        </div>
+        <div class="iconBox">
+          <img src="../../image/3tu_03.png">
+          <div>能力培养</div>
+        </div>
       </div>
       <div class="abilitys">能力养成馆</div>
       <div class="footer">
         <div class="fButton1" @click="goAbility">
-            <img src="../../image/footer1.png"/>
+          <img src="../../image/footer1.png">
         </div>
         <div class="fButton2">
-            <img src="../../image/footer2.png"/>
+          <img src="../../image/footer2.png">
         </div>
       </div>
     </div>
@@ -73,10 +73,10 @@ export default {
       return this.$refs.mySwiper.swiper;
     }
   },
-  methods:{
-      goAbility(){
-          this.$router.push({path:'/lesson'});
-      }
+  methods: {
+    goAbility() {
+      this.$router.push({ path: "/lesson" });
+    }
   }
 };
 </script>
@@ -85,6 +85,7 @@ export default {
   position: relative;
 }
 .fullImage {
+  width: 100%;
   position: absolute;
   z-index: -1;
 }
@@ -110,79 +111,80 @@ export default {
   color: white;
   font-size: 20px;
 }
-.swiperBox{
-    width: 90%;
-    margin: 4% 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10%;
+.swiperBox {
+  width: 79%;
+  margin: 4% 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10%;
 }
-.tabImage{
-    width: 100%;
+.tabImage {
+  width: 100%;
 }
-.iconButton{
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
+.iconButton {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 }
-.iconBox{
-    padding: 5px;
-    width: 100px;
-    height: 100px;
-    /* height: 20%; */
-    background-color: white;
-    border-radius: 20%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+.iconBox {
+  padding: 5px;
+  width: 100px;
+  height: 100px;
+  /* height: 20%; */
+  background-color: white;
+  border-radius: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-.iconBox img{
-    width: 60px;
-    height: 60px;
-    margin: 10px;
+.iconBox img {
+  width: 60px;
+  height: 60px;
+  margin: 10px;
 }
-.abilitys{
-    margin: 4% 0;
-    margin-left: 5%;
-    padding-left: 10px;
-    align-self: flex-start;
-    position: relative;
+.abilitys {
+  margin: 4% 0;
+  margin-left: 5%;
+  padding-left: 10px;
+  align-self: flex-start;
+  position: relative;
 }
-.abilitys::before{
-    position: absolute;
-    left: 0;
-    content: '';
-    display: block;
-    width: 5px;
-    height: 16px;
-    border-radius: 5px;
-    background-color: white;
+.abilitys::before {
+  position: absolute;
+  left: 0;
+  content: "";
+  display: block;
+  width: 5px;
+  height: 16px;
+  border-radius: 5px;
+  background-color: white;
 }
-.footer{
-    width: 80%;
-    padding: 4% 5%;
-    flex: 1;
-    display: flex;
-    justify-content: space-around;
-    flex-direction: column;
-    align-items: center;
-    background-color: #DAF3FF;
-    border-radius: 5%;
-    margin-bottom: 6%;
+.footer {
+  width: 80%;
+  padding: 4% 5%;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  background-color: #daf3ff;
+  border-radius: 5%;
 }
-.fButton1,.fButton1{
-    height: 100%;
-    position: relative;
+.fButton1,
+.fButton2 {
+  position: relative;
 }
-.footer img{
-    width: 100%;
+.fButton1{
+    margin-bottom: 4%;
 }
-.footer span{
-    position: absolute;
+.footer img {
+  width: 100%;
+}
+.footer span {
+  position: absolute;
 }
 </style>
 
